edce80aa08ff167483e93fd6d631e885238a08e4
Ayush Singh <ayushsingh01042003@gmail.com>
2024-04-03 21:36:36 +0530

    Express boilerplate

---
+++ Backend/src/server.ts
@@ -0,0 +1,16 @@
+import express, {Request, Response} from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const PORT: any = process.env.PORT || 3000;
+
+app.use('api/auth', authRoutes);
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello TS');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
