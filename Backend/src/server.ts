388e2965eaa3c94cdb36ce9f9c7275047bc3f777
Ayush Singh <ayushsingh01042003@gmail.com>
2024-04-04 19:44:16 +0530

    setup and mondoDB connection

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,16 +1,19 @@
 import express, {Request, Response} from "express";
 import dotenv from "dotenv";
+import authRoutes from './Routes/AuthRoutes'
+import connectToMongoDB from "./db/connectToMongo";
 
 dotenv.config();
 const app = express();
 const PORT: any = process.env.PORT || 3000;
 
-app.use('api/auth', authRoutes);
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello TS');
 });
 
 app.listen(PORT, () => {
+    connectToMongoDB();
     console.log(`Server running on port ${PORT}`);
 });
