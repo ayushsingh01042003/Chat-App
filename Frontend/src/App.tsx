2977f7e6b2e1584913ec0725c0425c598940a56c
Ayush Singh <ayushsingh01042003@gmail.com>
2024-04-25 18:39:55 +0530

    Add Router

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,22 @@
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "./components/theme-provider";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      hello world
-    </ThemeProvider>
+    <div>      
+      <ThemeProvider>
+        <Router>
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>    
+    </div>
   )
 }
 
