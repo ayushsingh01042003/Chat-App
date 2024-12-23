0e1ddbc6f12a3742b077bed55afba09b122cc104
Ayush Singh <ayushsingh01042003@gmail.com>
2024-12-23 22:37:33 +0530

    tsconfig.json, bug fixes

--- Backend/src/db/connectToMongo.ts
+++ Backend/src/db/connectToMongo.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async() => {
     try {
-        await mongoose.connect(`${process.env.MONGO_DB_URI}`);
+        await mongoose.connect(process.env.MONGO_DB_URI as string);
         console.log(`Connected to DB`);
     } catch(err) {
         console.log(`Error connecting to DB ${err}`);
--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,19 +1,18 @@
-import express from "express";
-import dotenv from "dotenv";
+import express, { Express } from "express";
 import authRoutes from './Routes/AuthRoutes'
 import messageRoutes from "./Routes/messageRoutes"
 import connectToMongoDB from "./db/connectToMongo";
 import userRoutes from './Routes/userRoutes';
 import cors from 'cors';
 
-dotenv.config();
-const app = express();
-const PORT: any = process.env.PORT || 3000;
+const app: Express = express();
+const PORT: number = parseInt(process.env.PORT as string) || 3000;
 
 app.use(express.json());
 app.use(cors({
     origin: 'http://localhost:5173'
 }));
+
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
